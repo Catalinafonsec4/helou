0298ecd273340efa32238b57c222de86ccaf2f18
Catalina fonseca <cata.f.imihuala@gmail.com>
2022-07-11 13:36:05 -0400

    subiendo pasos

---
+++ pasos para crear un repositorio.py
@@ -0,0 +1,29 @@
+"""
+ a continuacion se detallan los pasos para crear un repositorio en gitbub  ,enviando desde visual studio code
+
+paso 1- descargar e instalar git , va  a depender del sistema operativo que se tenga 
+
+paso.2-crear una cuenta en gitbub y luego crear un repositorio .este va a tener un url que se va utilizar
+    todo repositorio se debe colocar en publico , porque en privado se tiene que pagar
+
+paso3-abrir visual studio code,ir al boton de "view" y  dentro de el , la primera opcion "command pallete" y buscar "git clone"
+ que nos pedira la url del repositorio que creamos en gitbub.
+
+paso.4-nos abre el buscador de carpetas y eligimos donde queremos la carpeta del proyecto
+
+paso.5-aparece "would you like to open the cloned repository" ,le damos en open para abrie el repositorio vacio
+
+
+paso.6-creamos un archivo
+
+paso.7-en la casilla de mesage ,creamos un commit,pero todavia no aparece nada en gitbub,paara que se suba  hacemos clilc en push
+
+paso.8-actualizo en gitgub
+
+paso.9-ahora desde gitbub editamos el archivo y luego nos vamos a code para que aparesca ahi.vemos que abajo en la barra azul gay un boton de sincronizar 
+
+
+
+
+
+"""
